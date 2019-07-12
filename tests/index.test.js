7df65ed364623fa19b8ddf4c547338c525782032
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-11 23:13:17 -0700

    added question to function and passed test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,23 @@
 const { filterLetter } = require('../lib/index');
 
 describe('array function', () => {
-  it('returns an array', () => {
-    const words = ['lion', 'frog', 'panda'];
-    const wordArray = filterLetter(words);
+  // it('returns an array', () => {
+  //   const words = ['lion', 'frog', 'panda'];
+  //   const wordArray = filterLetter(words);
 
-    expect(wordArray).toEqual(expect.any(Array));
-  });
+  //   expect(wordArray).toEqual(expect.any(Array));
+  // });
   
-  it('takes out words with Ls', () => {
-    const arr = ['lemon', 'lime', 'orange', 'grapefruit'];
-    const newArr = filterLetter(arr);
-    expect(newArr).toEqual(['orange', 'grapefruit']);
+  // it('takes out words with Ls', () => {
+  //   const arr = ['lemon', 'lime', 'orange', 'grapefruit'];
+  //   const newArr = filterLetter(arr);
+  //   expect(newArr).toEqual(['orange', 'grapefruit']);
+  // });
+
+  it('takes remaining words and adds a question to it', () => {
+    const colors = ['blue', 'green', 'yellow', 'red'];
+    const questionArray = filterLetter(colors);
+    expect(questionArray).toEqual(['Do you like green?', 'Do you like red?'])
   });
 });
 
